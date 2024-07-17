261fe7703cc958bd954aafeec601d6814d6acd52
Knicholas Kennedy <knicholas.kennedy@gmail.com>
2024-07-17 18:38:56 -0400

    Corrected db2 deployment of ccm and ccd, made notation about JRE subdepedency
    Signed-off-by: Knicholas Kennedy <knicholas.kennedy@gmail.com>

--- env.sh
+++ env.sh
@@ -6,3 +6,7 @@ export SI_INSTANCEID=dev01
 # export SI_PREBUILTDB_FILE=/tmp/b2bidb6.2.tgz
 # export SI_DATASETUP_ENABLED=false
 # export SI_DATASETUP_UPGRADE=false
+# export CD_NODENAME=CDNODE01
+# export CD_NAMESPACE=sterling-cdcompany01-prod
+# export SCC_INSTANCEID=dev01
+# export SCC_PRODUCTS=CCM,CCD
