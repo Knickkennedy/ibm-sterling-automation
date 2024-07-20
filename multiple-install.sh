2215f6f231befb3f8c23a1317bbe0aecb0e6fecb
Knicholas Kennedy <knicholas.kennedy@gmail.com>
2024-07-20 16:27:21 -0400

    Added collections
    Signed-off-by: Knicholas Kennedy <knicholas.kennedy@gmail.com>

--- multiple-install.sh
+++ multiple-install.sh
@@ -3,6 +3,7 @@
 sudo dnf install -y python3.12 python3.12-pip java-1.8.0-openjdk.x86_64
 sudo alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
 sudo alternatives --set python3 /usr/bin/python3.12
+sudo ansible-galaxy collection install kubernetes.core
 
 ansible-playbook playbooks/install_prequisites.yml
 ansible-playbook playbooks/deploy_sb2b_prebuiltdb.yml & \
