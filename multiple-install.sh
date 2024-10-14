664b46573f0a1d6b52e7d834d32aebe97e104687
Knicholas Kennedy <knicholas.kennedy@ibm.com>
2024-10-14 10:26:20 -0400

    Update multiple-install.sh

--- multiple-install.sh
+++ multiple-install.sh
@@ -3,9 +3,9 @@
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-sudo dnf install -y ansible-core python3.12 python3.12-pip java-1.8.0-openjdk.x86_64
-sudo alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
-sudo alternatives --set python3 /usr/bin/python3.12
+sudo dnf install -y ansible-core python3.12 python3.12-pip java-1.8.0-openjdk.x86_64 python3-pip
+# sudo alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
+# sudo alternatives --set python3 /usr/bin/python3.12
 ansible-galaxy collection install kubernetes.core
 
 ansible-playbook playbooks/install_prequisites.yml
