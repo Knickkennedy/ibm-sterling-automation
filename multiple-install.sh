4648c57e38e9bff1b54fe81d0297dbefc0d7a1bd
halljb@us.ibm.com <halljb@us.ibm.com>
2024-11-05 13:57:53 -0500

    Commented out the kafka playbook that has errors
    Signed-off-by: halljb@us.ibm.com <halljb@us.ibm.com>

--- multiple-install.sh
+++ multiple-install.sh
@@ -16,6 +16,6 @@ ansible-playbook playbooks/deploy_sb2b_prebuiltdb.yml & \
 ansible-playbook playbooks/deploy_cd.yml & \
 #ansible-playbook playbooks/deploy_cdws.yml & \
 ansible-playbook playbooks/deploy_ssp.yml & \
-ansible-playbook playbooks/tools/kafka.yml & \
+#ansible-playbook playbooks/tools/kafka.yml & \
 ansible-playbook playbooks/tools/minio.yml &
 
